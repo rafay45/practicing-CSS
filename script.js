07ae2192988172cca1c03a116782cb10a6126aca
rafay45 <rafayy579@gmail.com>
2025-03-04 04:14:55 +0500

    more addition

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function add(event) {
    document.getElementById('input').value = ""
    }  
 }
+
+function one(){
+  let getOne = document.getElementById('card-one');
+  getOne.innerHTML = ` <img data-aos="zoom-in"  src="assets/gift-one.jpg" alt="">`
+}
+function two(){
+  let getOne = document.getElementById('card-two');
+  getOne.innerHTML = ` <img data-aos="zoom-in"  src="assets/gift-two.jpg" alt="">`
+}
+function three(){
+  let getOne = document.getElementById('card-three');
+  getOne.innerHTML = ` <img data-aos="zoom-in"  src="assets/gift-three.jpg" alt="">`
+}
+function four(){
+  let getOne = document.getElementById('card-four');
+  getOne.innerHTML = ` <img data-aos="zoom-in"  src="assets/gift-four.jpg" alt="">`
+}
