e5af693a7832e698f48c118ffad4d44b7b7fb578
rafay45 <rafayy579@gmail.com>
2025-03-06 18:20:14 +0500

    more addition

--- script.js
+++ script.js
@@ -1,29 +1,71 @@
 
 function add(event) {
-   let getValue = document.getElementById('input').value; 
-   event.preventDefault();
-   if(getValue === "951235"){
-     window.location.href = "home.html"
-   }
-   else{
+  let getValue = document.getElementById('input').value;
+  event.preventDefault();
+  if (getValue === "951235") {
+    window.location.href = "home.html"
+  }
+  else {
     document.getElementById('alert').style.display = "block"
-   document.getElementById('input').value = ""
-   }  
+    document.getElementById('input').value = ""
+  }
 }
 
-function one(){
+function one() {
   let getOne = document.getElementById('card-one');
   getOne.innerHTML = ` <img data-aos="zoom-in"  src="assets/gift-one.jpg" alt="">`
 }
-function two(){
+function two() {
   let getOne = document.getElementById('card-two');
   getOne.innerHTML = ` <img data-aos="zoom-in"  src="assets/gift-two.jpg" alt="">`
 }
-function three(){
+function three() {
   let getOne = document.getElementById('card-three');
   getOne.innerHTML = ` <img data-aos="zoom-in"  src="assets/gift-three.jpg" alt="">`
 }
-function four(){
+function four() {
   let getOne = document.getElementById('card-four');
   getOne.innerHTML = ` <img data-aos="zoom-in"  src="assets/gift-four.jpg" alt="">`
+}
+function onebtn() {
+  let quesOne = document.getElementById('quesOne')
+  if (quesOne.children[1].innerHTML === "Yes") {
+    quesOne.innerHTML = `<p>Hayeee I Love You Too🙈</p>`
+  }
+}
+function twobtn() {
+  let quesOne = document.getElementById('quesOne')
+  if (quesOne.children[2].innerHTML === "No") {
+    quesOne.innerHTML = `<p>Jaan☹️</p>`
+  }
+}
+function threebtn() {
+  let quesOne = document.getElementById('quesTwo')
+  if (quesOne.children[1].innerHTML === "Yes") {
+    quesOne.innerHTML = `<p>Yes, I am Your Husband🥳</p>`
+  }
+}
+function fourthbtn() {
+  let quesOne = document.getElementById('quesTwo')
+  if (quesOne.children[2].innerHTML === "No") {
+    quesOne.innerHTML = `<p>Hooo😱</p>`
+  }
+}
+function fivebtn() {
+  let quesOne = document.getElementById('quesThree')
+  if (quesOne.children[1].innerHTML === "Yes") {
+    quesOne.innerHTML = `<p>hehehehehe🤭🙈</p>`
+  }
+}
+function sixbtn() {
+  let quesOne = document.getElementById('quesThree')
+  if (quesOne.children[2].innerHTML === "No") {
+    quesOne.innerHTML = `<p>Aennnnn I am your Pookie😭</p>`
+  }
+}
+function sixbtn() {
+  let quesOne = document.getElementById('quesThree')
+  if (quesOne.children[2].innerHTML === "No") {
+    quesOne.innerHTML = `<p>Aennnnn I am your Pookie😭</p>`
+  }
 }
